a6cdce8dda2721de5b7a0aebaf051ed02a33d431
rmarro <rmarro47@gmail.com>
2018-01-13 18:02:50 -0600

    added initial files, created table and form

---
+++ assets/scheduleLogic.js
@@ -0,0 +1,16 @@
+// Initialize firebase
+// Create button for adding new train
+// Retrieve all train info and put in table
+// Calculate next arrival time and minutes until
+
+
+// Initialize firebase
+var config = {
+    apiKey: "AIzaSyBz0xaZ3-V84TxSX49RWmgN_ozQ-KB5Xus",
+    authDomain: "train-scheduler-d37aa.firebaseapp.com",
+    databaseURL: "https://train-scheduler-d37aa.firebaseio.com",
+    projectId: "train-scheduler-d37aa",
+    storageBucket: "train-scheduler-d37aa.appspot.com",
+    messagingSenderId: "608347940362"
+  };
+  firebase.initializeApp(config);
